fe0f674b39d6be578e8b4161f9bf7bf2d608da61
syuhei176 <syuhei176@gmail.com>
2023-10-27 03:46:29 +0900

    add uniswap settlement address mapping

--- src/settlement/index.ts
+++ src/settlement/index.ts
@@ -1,9 +1,11 @@
 import { BigNumber, ethers } from "ethers";
 
+import { UNISWAP_SETTLEMENT_MAPPING } from "../constants";
+
 const UNISWAP_SETTLEMENT_DATA_ABI = [
   "tuple(" +
-  ["bytes", "uint256", "address", "address", "uint256"].join(",") +
-  ")",
+    ["bytes", "uint256", "address", "address", "uint256"].join(",") +
+    ")",
 ];
 
 interface SettlementData {
@@ -16,28 +18,40 @@ abstract class BaseSettlement {
 }
 
 export class UniswapSettlement extends BaseSettlement {
+  settlementContractAddress: string;
+
   constructor(
+    public chainId: number,
     public path: string,
     public amountOutMinimumOrInMaximum: BigNumber,
     public quoteTokenAddress: string,
     public baseTokenAddress: string,
-    public fee: BigNumber
+    public fee: BigNumber,
+    settlementContractAddress?: string
   ) {
     super();
+
+    if (settlementContractAddress) {
+      this.settlementContractAddress = settlementContractAddress;
+    } else {
+      this.settlementContractAddress = UNISWAP_SETTLEMENT_MAPPING[chainId];
+    }
   }
 
   serialize(): SettlementData {
     const abiCoder = new ethers.utils.AbiCoder();
 
     return {
-      settlementContractAddress: "",
-      encodedData: abiCoder.encode(UNISWAP_SETTLEMENT_DATA_ABI, [[
-        this.path,
-        this.amountOutMinimumOrInMaximum,
-        this.quoteTokenAddress,
-        this.baseTokenAddress,
-        this.fee,
-      ]]),
+      settlementContractAddress: this.settlementContractAddress,
+      encodedData: abiCoder.encode(UNISWAP_SETTLEMENT_DATA_ABI, [
+        [
+          this.path,
+          this.amountOutMinimumOrInMaximum,
+          this.quoteTokenAddress,
+          this.baseTokenAddress,
+          this.fee,
+        ],
+      ]),
     };
   }
 }
